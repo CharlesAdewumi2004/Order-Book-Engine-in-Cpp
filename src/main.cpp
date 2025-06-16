be8f71dc78415ac80343ed7dba5662f66819aaee
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2025-06-16 13:06:33 +0100

    setting up and testing catch2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    std::cout << "OrderBook CLI starting...\n";
+    std::cout << "2 + 3 = " << add(2, 3) << std::endl;
     return 0;
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,2 +1,15 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
+
+
+// This replicates the function from main.cpp
+int add(int a, int b) {
+    return a + b;
+}
+
+TEST_CASE("Addition works", "[add]") {
+    REQUIRE(add(1, 1) == 2);
+    REQUIRE(add(2, 3) == 5);
+    REQUIRE(add(-1, 1) == 0);
+    REQUIRE(add(0, 0) == 0);
+}
